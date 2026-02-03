98839f9aeda2fe8f0a935bf17b762d62a3b40b8e
sofiaasena <sofiiaseena@gmail.com>
2026-02-03 13:58:34 -0300

    Primeiro commit de 2026

---
+++ Programação Back End/POO/index.php
@@ -0,0 +1,3 @@
+<?php 
+echo "<h1>Aulas de programação orientadas a objeto</h1>";
+?>
---
+++ Programação Back End/index.php
@@ -0,0 +1,4 @@
+<?php 
+echo "<h1>Página principal do meu servidor</h1>";
+?>
+<a href = "/POO/index.php"> Aula de POO </a>
